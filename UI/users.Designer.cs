2a6c4aaa7e6b8d38da4243a056de53ecb11b446b
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-02-25 15:50:33 +0300

    user form design

--- UI/users.Designer.cs
+++ UI/users.Designer.cs
@@ -30,7 +30,20 @@
         {
             panelTop = new Panel();
             lblusersForm = new Label();
+            lblProfPic = new Label();
+            label2 = new Label();
+            lbladdress = new Label();
+            lblContact = new Label();
+            lblPwd = new Label();
+            lblUsername = new Label();
+            lblmail = new Label();
+            lblFullname = new Label();
+            lblUserID = new Label();
+            pictureBox1 = new PictureBox();
+            btnSI = new Button();
+            txtUserID = new TextBox();
             panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panelTop
@@ -52,9 +65,137 @@
             lblusersForm.ForeColor = SystemColors.ControlText;
             lblusersForm.Location = new Point(544, 31);
             lblusersForm.Name = "lblusersForm";
-            lblusersForm.Size = new Size(97, 35);
+            lblusersForm.Size = new Size(214, 35);
             lblusersForm.TabIndex = 0;
-            lblusersForm.Text = "Users";
+            lblusersForm.Text = "Manage Users";
+            // 
+            // lblProfPic
+            // 
+            lblProfPic.AutoSize = true;
+            lblProfPic.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblProfPic.ForeColor = SystemColors.Desktop;
+            lblProfPic.Location = new Point(12, 92);
+            lblProfPic.Name = "lblProfPic";
+            lblProfPic.Size = new Size(136, 24);
+            lblProfPic.TabIndex = 1;
+            lblProfPic.Text = "Profile Picture";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Desktop;
+            label2.Location = new Point(676, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(136, 24);
+            label2.TabIndex = 3;
+            label2.Text = "Profile Picture";
+            // 
+            // lbladdress
+            // 
+            lbladdress.AutoSize = true;
+            lbladdress.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lbladdress.ForeColor = SystemColors.Desktop;
+            lbladdress.Location = new Point(12, 505);
+            lbladdress.Name = "lbladdress";
+            lbladdress.Size = new Size(85, 24);
+            lbladdress.TabIndex = 4;
+            lbladdress.Text = "Address";
+            // 
+            // lblContact
+            // 
+            lblContact.AutoSize = true;
+            lblContact.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblContact.ForeColor = SystemColors.Desktop;
+            lblContact.Location = new Point(20, 450);
+            lblContact.Name = "lblContact";
+            lblContact.Size = new Size(77, 24);
+            lblContact.TabIndex = 5;
+            lblContact.Text = "Contact";
+            // 
+            // lblPwd
+            // 
+            lblPwd.AutoSize = true;
+            lblPwd.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblPwd.ForeColor = SystemColors.Desktop;
+            lblPwd.Location = new Point(20, 409);
+            lblPwd.Name = "lblPwd";
+            lblPwd.Size = new Size(97, 24);
+            lblPwd.TabIndex = 6;
+            lblPwd.Text = "Password";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblUsername.ForeColor = SystemColors.Desktop;
+            lblUsername.Location = new Point(28, 367);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(98, 24);
+            lblUsername.TabIndex = 7;
+            lblUsername.Text = "Username";
+            // 
+            // lblmail
+            // 
+            lblmail.AutoSize = true;
+            lblmail.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblmail.ForeColor = SystemColors.Desktop;
+            lblmail.Location = new Point(38, 343);
+            lblmail.Name = "lblmail";
+            lblmail.Size = new Size(59, 24);
+            lblmail.TabIndex = 8;
+            lblmail.Text = "Email";
+            // 
+            // lblFullname
+            // 
+            lblFullname.AutoSize = true;
+            lblFullname.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblFullname.ForeColor = SystemColors.Desktop;
+            lblFullname.Location = new Point(12, 294);
+            lblFullname.Name = "lblFullname";
+            lblFullname.Size = new Size(97, 24);
+            lblFullname.TabIndex = 9;
+            lblFullname.Text = "Full Name";
+            // 
+            // lblUserID
+            // 
+            lblUserID.AutoSize = true;
+            lblUserID.Font = new Font("Harrington", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblUserID.ForeColor = SystemColors.Desktop;
+            lblUserID.Location = new Point(20, 219);
+            lblUserID.Name = "lblUserID";
+            lblUserID.Size = new Size(75, 24);
+            lblUserID.TabIndex = 10;
+            lblUserID.Text = "User ID";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(177, 103);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(122, 82);
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // btnSI
+            // 
+            btnSI.Cursor = Cursors.Hand;
+            btnSI.ForeColor = SystemColors.Desktop;
+            btnSI.Location = new Point(325, 156);
+            btnSI.Name = "btnSI";
+            btnSI.Size = new Size(161, 41);
+            btnSI.TabIndex = 12;
+            btnSI.Text = "Select image";
+            btnSI.UseVisualStyleBackColor = true;
+            // 
+            // txtUserID
+            // 
+            txtUserID.BorderStyle = BorderStyle.None;
+            txtUserID.Cursor = Cursors.No;
+            txtUserID.Location = new Point(174, 213);
+            txtUserID.Name = "txtUserID";
+            txtUserID.ReadOnly = true;
+            txtUserID.Size = new Size(312, 28);
+            txtUserID.TabIndex = 13;
             // 
             // users
             // 
@@ -62,6 +203,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ScrollBar;
             ClientSize = new Size(1200, 700);
+            Controls.Add(txtUserID);
+            Controls.Add(btnSI);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblUserID);
+            Controls.Add(lblFullname);
+            Controls.Add(lblmail);
+            Controls.Add(lblUsername);
+            Controls.Add(lblPwd);
+            Controls.Add(lblContact);
+            Controls.Add(lbladdress);
+            Controls.Add(label2);
+            Controls.Add(lblProfPic);
             Controls.Add(panelTop);
             Font = new Font("Harrington", 13.8F, FontStyle.Italic, GraphicsUnit.Point, 0);
             ForeColor = SystemColors.Control;
@@ -72,12 +225,26 @@
             Text = "users";
             panelTop.ResumeLayout(false);
             panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel panelTop;
         private Label lblusersForm;
+        private Label lblProfPic;
+        private Label label2;
+        private Label lbladdress;
+        private Label lblContact;
+        private Label lblPwd;
+        private Label lblUsername;
+        private Label lblmail;
+        private Label lblFullname;
+        private Label lblUserID;
+        private PictureBox pictureBox1;
+        private Button btnSI;
+        private TextBox txtUserID;
     }
 }
