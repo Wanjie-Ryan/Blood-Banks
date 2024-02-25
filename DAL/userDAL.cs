1364b4b3532ebbf86be97c17d2480127c43bef00
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-02-25 17:49:23 +0300

    db functionality in progress

--- DAL/userDAL.cs
+++ DAL/userDAL.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Blood_Banks.BLL;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -49,6 +50,69 @@ namespace Blood_Banks.DAL
 
             return dt;
 
+        }
+
+        public bool Insert(userBLL u)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection();
+
+            try
+            {
+                string sql = "INSERT INTO users (user_id, username, email, password, full_name, contact, address, added_date, image_name) VALUES (@user_id, @username, @email, @password, @full_name, @contact, @address, @added_date, @image_name)";
+
+                // create am sql command to pass the value in the query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                conn.Open();
+
+                // create the parameters to get value from the UI and pass it 
+                cmd.Parameters.AddWithValue("@user_id", u.user_id);
+                cmd.Parameters.AddWithValue("@username", u.username);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@password", u.password);
+                cmd.Parameters.AddWithValue("@full_name", u.full_name);
+                cmd.Parameters.AddWithValue("@contact", u.contact);
+                cmd.Parameters.AddWithValue("@address", u.address);
+                cmd.Parameters.AddWithValue("@added_date", u.added_date);
+                cmd.Parameters.AddWithValue("@image_name", u.image_name);
+
+                //open db connection
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+
+        //update functionality
+
+        public bool Update(userBLL u)
+        {
+            bool isSuccess = false;
+
+
+
+            return isSuccess;
         }
     }
 }
