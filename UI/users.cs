2fef430a61e59dbaf64deb0ad8e8f27bcdbfd68a
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-02-27 13:27:15 +0300

    database connections done

--- UI/users.cs
+++ UI/users.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Blood_Banks.BLL;
+using MySql.Data.MySqlClient;
+
+ using Blood_Banks.DAL;
+//using Blood_Banks.Program;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +22,42 @@ namespace Blood_Banks.UI
             InitializeComponent();
         }
 
+         userBLL u = new userBLL();
+         userDAL dal = new userDAL();
+        //Program pg = new Program();
+        string imageName = "no-image.png";
         private void pbusersClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {                    
+            u.image_name = imageName;
+            u.full_name = txtFullname.Text;
+            u.email = txtmail.Text;
+            u.username = txtUsername.Text;
+            u.password = txtPassword.Text;
+            u.contact = txtContact.Text;
+            u.address = txtAddress.Text;
+            u.added_date = DateTime.Now;
+
+            // adding values from the UI to the DB
+            bool Success = dal.Insert(u);
+
+            if (Success == true)
+            {
+                // data or user added successfully
+                MessageBox.Show("User was added successfully", "Insertion Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Failed to add new user", "Insertion Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+
         }
     }
 }
