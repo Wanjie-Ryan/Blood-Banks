91bb98e58209870ca5d0ed5d601e33e188a0af00
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-09 15:41:19 +0300

    database manipulation

--- DAL/donorDAL.cs
+++ DAL/donorDAL.cs
@@ -45,10 +45,97 @@ namespace Blood_Banks.DAL
 
             using (MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
             {
+                try
+                {
+                    string sql = "INSERT INTO donors (donor_id, first_name, last_name, email, contact, gender, address, blood_group, added_date, image_name, added_by) VALUES (@donor_id, @first_name, @last_name, @email, @contact, @gender, @address, @blood_group, @added_date, @image_name, @added_by)";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
 
+                    cmd.Parameters.AddWithValue("@donor_id", b.donor_id);
+                    cmd.Parameters.AddWithValue("@first_name", b.first_name);
+                    cmd.Parameters.AddWithValue("@last_name", b.last_name);
+                    cmd.Parameters.AddWithValue("@email", b.email);
+                    cmd.Parameters.AddWithValue("@contact", b.contact);
+                    cmd.Parameters.AddWithValue("@gender", b.gender);
+                    cmd.Parameters.AddWithValue("@address", b.address);
+                    cmd.Parameters.AddWithValue("@blood_group", b.blood_group);
+                    cmd.Parameters.AddWithValue("@added_date", b.added_date);
+                    cmd.Parameters.AddWithValue("@image_name", b.image_name);
+                    cmd.Parameters.AddWithValue("@added_by", b.added_by);
+
+                    conn.Open();
+
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        isSuccess = true;
+                    }
+                    else
+                    {
+                        isSuccess = false;
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
             }
 
                 return isSuccess;
+        }
+
+        public bool Update(donorsBLL b)
+        {
+            bool isSuccess = false;
+
+            using (MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+                    string sql = "UPDATE donors SET first_name =@first_name, last_name =@last_name, email=@email,contact=@contact, gender=@gender, address=@address, blood_group= @blood_group, added_date=@added_date, image_name=@image_name, added_by=@added_by WHERE donor_id=@donor_id";
+
+                    MySqlCommand cmd = new MySqlCommand();
+
+                    cmd.Parameters.AddWithValue("@donor_id", b.donor_id);
+                    cmd.Parameters.AddWithValue("@first_name", b.first_name);
+                    cmd.Parameters.AddWithValue("@last_name", b.last_name);
+                    cmd.Parameters.AddWithValue("@email", b.email);
+                    cmd.Parameters.AddWithValue("@contact", b.contact);
+                    cmd.Parameters.AddWithValue("@gender", b.gender);
+                    cmd.Parameters.AddWithValue("@address", b.address);
+                    cmd.Parameters.AddWithValue("@blood_group", b.blood_group);
+                    cmd.Parameters.AddWithValue("@added_date", b.added_date);
+                    cmd.Parameters.AddWithValue("@image_name", b.image_name);
+                    cmd.Parameters.AddWithValue("@added_by", b.added_by);
+
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        isSuccess = true;
+                    }
+                    else
+                    {
+                        isSuccess = false;
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return isSuccess;
         }
     }
 }
