584f08b65fcf031919f3b148eda4a0327c620017
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-13 15:54:27 +0300

    login database manipulation

---
+++ DAL/loginDAL.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Blood_Banks.BLL;
+
+
+namespace Blood_Banks.DAL
+{
+    internal class loginDAL
+    {
+        public bool LoginCheck(loginBLL l)
+        {
+            bool isSuccess = false;
+
+            using (MySqlConnection conn = new MySqlConnection(Program.GetConnectionString()))
+            {
+                try
+                {
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+
+            return isSuccess;
+        }
+    }
+}
