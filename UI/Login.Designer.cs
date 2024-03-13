5e60c7eed7a0edca8d133882a0518c12266eb67e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-13 14:52:15 +0300

    login form design

--- UI/Login.Designer.cs
+++ UI/Login.Designer.cs
@@ -28,7 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            pbLogin = new PictureBox();
+            lblLoginTitle = new Label();
+            lblTitleLogin = new Label();
+            lblUsername = new Label();
+            ((System.ComponentModel.ISupportInitialize)pbLogin).BeginInit();
             SuspendLayout();
+            // 
+            // pbLogin
+            // 
+            pbLogin.Image = (Image)resources.GetObject("pbLogin.Image");
+            pbLogin.Location = new Point(180, 102);
+            pbLogin.Name = "pbLogin";
+            pbLogin.Size = new Size(258, 208);
+            pbLogin.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbLogin.TabIndex = 0;
+            pbLogin.TabStop = false;
+            // 
+            // lblLoginTitle
+            // 
+            lblLoginTitle.AutoSize = true;
+            lblLoginTitle.Font = new Font("Harrington", 28.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblLoginTitle.Location = new Point(159, 326);
+            lblLoginTitle.Name = "lblLoginTitle";
+            lblLoginTitle.Size = new Size(301, 55);
+            lblLoginTitle.TabIndex = 1;
+            lblLoginTitle.Text = "Blood Banks";
+            // 
+            // lblTitleLogin
+            // 
+            lblTitleLogin.AutoSize = true;
+            lblTitleLogin.Location = new Point(180, 392);
+            lblTitleLogin.Name = "lblTitleLogin";
+            lblTitleLogin.Size = new Size(251, 21);
+            lblTitleLogin.TabIndex = 2;
+            lblTitleLogin.Text = "Management System -Login";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(620, 102);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(97, 21);
+            lblUsername.TabIndex = 3;
+            lblUsername.Text = "Username";
             // 
             // Login
             // 
@@ -36,15 +80,26 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ScrollBar;
             ClientSize = new Size(900, 600);
+            Controls.Add(lblUsername);
+            Controls.Add(lblTitleLogin);
+            Controls.Add(lblLoginTitle);
+            Controls.Add(pbLogin);
             Font = new Font("Harrington", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(4, 3, 4, 3);
             Name = "Login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pbLogin).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pbLogin;
+        private Label lblLoginTitle;
+        private Label lblTitleLogin;
+        private Label lblUsername;
     }
 }
